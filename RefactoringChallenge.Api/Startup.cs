a64dec09acafc9df15199eeb560d9f5102f8362d
caner <caneraras@msn.com>
2022-11-30 09:32:28 +0000

    * Required Packages

--- RefactoringChallenge.Api/Startup.cs
+++ RefactoringChallenge.Api/Startup.cs
@@ -22,7 +22,7 @@ namespace RefactoringChallenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<NorthwindDbContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+            services.AddDbContext<NorthwindDbContext>(options => options.UseInMemoryDatabase("Technical-Challenge"));
 
             services.AddSingleton(TypeAdapterConfig.GlobalSettings);
             services.AddScoped<IMapper, ServiceMapper>();
